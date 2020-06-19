ba5f3125b96f2b58905afb8029e5b064abf66a9f
Byte-Block <Sergio.Rajic@gmail.com>
2020-06-19 14:27:53 +0200

    Search function implemented

--- src/services/HeroService.js
+++ src/services/HeroService.js
@@ -1,7 +1,7 @@
 import { Hero } from "../entities/HeroEntity";
 const api_Key = "6df71823e082a7c62f84539374749aec";
 
-const HeroService = () => {
+const HeroesService = () => {
   return fetch(
     `http://gateway.marvel.com/v1/public/characters?apikey=${api_Key}`
   )
@@ -12,10 +12,14 @@ const HeroService = () => {
     );
 };
 
-const HeroesNameStartsWithService = () => {
-  return fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=iron&apikey=`)
-}
+const HeroesNameStartsWithService = (name) => {
+  return fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&apikey=${api_Key}`)
+    .then(response => response.json())
+    .then(heroes => heroes.data.results)
+    .then(heroes => heroes.map(hero => new Hero(hero.name, hero.thumbnail.path, hero.id))
+    );
+};
 
-export { HeroService };
+export { HeroesService, HeroesNameStartsWithService };
 
 //fixing services folder name to lower caps on remote master
