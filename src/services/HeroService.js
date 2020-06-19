9227edb5849abf1974b3beab48d65c8f740a7e99
Byte-Block <Sergio.Rajic@gmail.com>
2020-06-19 15:58:21 +0200

    Components composed

--- src/services/HeroService.js
+++ src/services/HeroService.js
@@ -1,25 +1,29 @@
 import { Hero } from "../entities/HeroEntity";
 const api_Key = "6df71823e082a7c62f84539374749aec";
 
-const HeroesService = () => {
-  return fetch(
-    `http://gateway.marvel.com/v1/public/characters?apikey=${api_Key}`
-  )
-    .then((response) => response.json())
-    .then((response) => response.data.results)
-    .then((response) =>
-      response.map((hero) => new Hero(hero.name, hero.thumbnail.path, hero.id))
-    );
-};
+class HeroService {
+  static fetchAll() {
+    return fetch(
+      `http://gateway.marvel.com/v1/public/characters?apikey=${api_Key}`
+    )
+      .then((response) => response.json())
+      .then((response) => response.data.results)
+      .then((response) =>
+        response.map((hero) => new Hero(hero.name, hero.thumbnail.path, hero.id))
+      );
+  }
 
-const HeroesNameStartsWithService = (name) => {
-  return fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&apikey=${api_Key}`)
-    .then(response => response.json())
-    .then(heroes => heroes.data.results)
-    .then(heroes => heroes.map(hero => new Hero(hero.name, hero.thumbnail.path, hero.id))
-    );
-};
+  static fetch(id) {
+    // fetch single hero
+  }
 
-export { HeroesService, HeroesNameStartsWithService };
+  static search(name) {
+    return fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&apikey=${api_Key}`)
+      .then(response => response.json())
+      .then(heroes => heroes.data.results)
+      .then(heroes => heroes.map(hero => new Hero(hero.name, hero.thumbnail.path, hero.id))
+      );
+  }
+}
 
-//fixing services folder name to lower caps on remote master
+export { HeroService };
