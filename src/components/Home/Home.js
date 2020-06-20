a9c0da97e05b9f6705571bfcfb28b79ac042fe6f
Byte-Block <Sergio.Rajic@gmail.com>
2020-06-20 23:19:55 +0200

    Axios implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,55 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import { Row, Col } from 'react-materialize';
+import { Row, Col } from "react-materialize";
 
-import { HeroCard } from '../HeroCard/HeroCard';
-import { SearchBar } from '../SearchBar/SearchBar';
+import { HeroCard } from "../HeroCard/HeroCard";
+import { SearchBar } from "../SearchBar/SearchBar";
 import { HeroService } from "../../services/HeroService";
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            heroes: [],
-        }
+    this.state = {
+      heroes: [],
+    };
 
-        this.searchHeroes = this.searchHeroes.bind(this);
-    }
+    this.searchHeroes = this.searchHeroes.bind(this);
+  }
 
-    componentDidMount() {
-        this.fetchHeroes();
-    }
+  componentDidMount() {
+    this.fetchHeroes();
+  }
 
-    fetchHeroes() {
-        HeroService.fetchAll().then((heroes) => {
-            this.setState({ heroes });
-        });
-    }
+  fetchHeroes() {
+    HeroService.fetchAll().then((heroes) => {
+      this.setState({ heroes });
+    });
+  }
 
-    searchHeroes(query) {
-        HeroService.search(query).then((heroes) => {
-            this.setState({ heroes });
-        });
-    }
+  searchHeroes(query) {
+    HeroService.search(query).then((heroes) => {
+      this.setState({ heroes });
+    });
+  }
 
-    render() {
-        return (
-            <>
-                <SearchBar searchHeroes={this.searchHeroes} />
-                <Row>
-                    <Col s={9}>
-                        {
-                            this.state.heroes.map((hero) => (
-                                <HeroCard name={hero.name} avatar={hero.avatar} key={hero.id} />
-                            ))
-                        }
-                    </Col>
-                </Row>
-            </>
-        );
-    }
-
+  render() {
+    return (
+      <>
+        <SearchBar searchHeroes={this.searchHeroes} />
+        <Row>
+          <Col s={9}>
+            {this.state.heroes.map((hero) => (
+              <HeroCard name={hero.name} avatar={hero.avatar} key={hero.id} />
+            ))}
+          </Col>
+        </Row>
+      </>
+    );
+  }
 }
 
 export { Home };
--- src/services/HeroService.js
+++ src/services/HeroService.js
@@ -1,15 +1,16 @@
 import { Hero } from "../entities/HeroEntity";
+import axios from "axios";
+
 const api_Key = "6df71823e082a7c62f84539374749aec";
 
 class HeroService {
   static fetchAll() {
-    return fetch(
-      `http://gateway.marvel.com/v1/public/characters?apikey=${api_Key}`
-    )
-      .then((response) => response.json())
-      .then((response) => response.data.results)
-      .then((response) =>
-        response.map((hero) => new Hero(hero.name, hero.thumbnail.path, hero.id))
+    return axios
+      .get(`https://gateway.marvel.com/v1/public/characters?apikey=${api_Key}`)
+      .then((heroes) =>
+        heroes.data.data.results.map(
+          (hero) => new Hero(hero.name, hero.thumbnail.path, hero.id)
+        )
       );
   }
 
@@ -18,10 +19,14 @@ class HeroService {
   }
 
   static search(name) {
-    return fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&apikey=${api_Key}`)
-      .then(response => response.json())
-      .then(heroes => heroes.data.results)
-      .then(heroes => heroes.map(hero => new Hero(hero.name, hero.thumbnail.path, hero.id))
+    return axios
+      .get(
+        `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&apikey=${api_Key}`
+      )
+      .then((heroes) =>
+        heroes.data.data.results.map(
+          (hero) => new Hero(hero.name, hero.thumbnail.path, hero.id)
+        )
       );
   }
 }
